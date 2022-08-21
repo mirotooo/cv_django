01193a29e5df17cc99767a8d3507958b69f1cff8
Suyeon <92787722+mirotooo@users.noreply.github.com>
2022-08-21 21:46:16 +0200

    CV +gabarit

--- cv/views.py
+++ cv/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import json
 
 # Create your views here.
+
+
 def index(request):
-        return HttpResponse("test")
+    f = open('cv/json/infos.json', 'r')
+
+    return render(request, "cv/index.html", json.load(f))
--- main/settings.py
+++ main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cv',
+    'portfolio'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'cv_django.urls'
+ROOT_URLCONF = 'main.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +70,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'cv_django.wsgi.application'
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+WSGI_APPLICATION = 'main.wsgi.application'
 
 
 # Database
@@ -116,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"), 
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

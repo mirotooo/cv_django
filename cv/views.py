f9aa7bbf06a8a2cdac70dfc1be4357f6a5c37c5e
Sanja <sanja@Skynet.be>
2022-08-22 13:32:32 +0200

    static json

--- cv/views.py
+++ cv/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import json
+from django.conf import settings
+import os
+
 
 # Create your views here.
 
 
 def index(request):
-    f = open('cv/json/infos.json', 'r')
+    f = open(os.path.join(settings.BASE_DIR, 'static/json/infos.json'), 'r')
 
     return render(request, "cv/index.html", json.load(f))
